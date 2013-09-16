1048206bf26ead58a17bf09930d92ecc371e632d
akmhoque <akmhoque@gmail.com>
2013-09-16 11:26:42 -0500

    Cache ccndid (ospfn.*)

--- ospfn/ccn_fib.h
+++ ospfn/ccn_fib.h
@@ -7,8 +7,8 @@
 #define OP_REG  0
 #define OP_UNREG 1
 
-void init_data(struct ccn_charbuf *local_scope_template);
-int get_ccndid(struct ccn *h, struct ccn_charbuf *local_scope_template,unsigned char *ccndid);
+extern void init_data(struct ccn_charbuf *local_scope_template);
+extern int get_ccndid(struct ccn *h, struct ccn_charbuf *local_scope_template,unsigned char *ccndid);
 extern int add_ccn_face(struct ccn *h, const char *uri, const char *address, const unsigned int p);
 extern int delete_ccn_face(struct ccn *h, const char *uri, const char *address, const unsigned int p);
 #endif
